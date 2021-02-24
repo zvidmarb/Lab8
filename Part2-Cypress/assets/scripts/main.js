711617946dd547ad43faa77fb526f829d9681494
Chau Vu <chv004@ucsd.edu>
2021-02-23 17:20:15 -0800

    Added Party Horn

--- Part2-Cypress/assets/scripts/main.js
+++ Part2-Cypress/assets/scripts/main.js
@@ -6,6 +6,7 @@ let numInput = document.getElementById('volume-number');
 let volumeIcon = document.getElementById('volume-image');
 let airHorn = document.getElementById('radio-air-horn');
 let carHorn = document.getElementById('radio-car-horn');
+let partyHorn = document.getElementById('radio-party-horn');
 let hornSound = document.getElementById('horn-sound');
 let soundImage = document.getElementById('sound-image');
 let honkBtn = document.getElementById('honk-btn');
@@ -54,6 +55,11 @@ airHorn.addEventListener('change', () => {
 carHorn.addEventListener('change', () => {
   soundImage.src = "./assets/media/images/car.svg";
   hornSound.src = "./assets/media/audio/car-horn.mp3";
+});
+
+partyHorn.addEventListener('change', () => {
+  soundImage.src = "./assets/media/images/party-horn.svg";
+  hornSound.src = "./assets/media/audio/party-horn.mp3";
 });
 
 form.addEventListener('submit', (e) => {
